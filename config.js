b55870cd78d7fedb4ba03afece10229c6fbfdf6b
DarkeNima <naviyaoficial@gmail.com>
2025-12-12 13:37:57 +0530

    DARK NAVIYA

---
+++ config.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "NAVIYA-MD=BpbM2GEWfCDbXhv",
+MONGODB: process.env.MONGODB || "mongodb+srv://cisoler713:lTVe7bnXQ8ahr5Ks@cluster0.gomfl.mongodb.net",
+OMDB_API_KEY: process.env.OMDB_API_KEY || "8748dc2e",
+AUTO_RECORDING:process.env.AUTO_RECORDING || "true",
+AUTO_NEWS:process.env.AUTO_NEWS || "false",
+AUTO_STATUS_LIKE: process.env.AUTO_STATUS_LIKE || "true" 
+};
+
+
+
+
+
+
