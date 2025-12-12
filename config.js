87e7ded2d92983233bbf2cce611b8ccf849c22ee
DarkeNima <naviyaoficial@gmail.com>
2025-12-12 15:58:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "NAVIYA-MD=KM0D1Dia#BrpA2PRyF4dMZW4DYfApaAZITRM9k2ngj7g724JP38A",
+SESSION_ID: process.env.SESSION_ID || "NAVIYA-MD=fVVX3BCb#9aJ-fkWVdjic36KyCJ_otnufDJRCR1jBFF92GdOXs7Y",
 MONGODB: process.env.MONGODB || "mongodb+srv://darknaviya78_db_user:mbWTl6vbPt8J9Y0S@cluster0.jwwyvwc.mongodb.net",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "8748dc2e",
 AUTO_RECORDING:process.env.AUTO_RECORDING || "true",
